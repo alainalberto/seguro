03bf651c20584b68b0bc35d36f4d809096b7be5b
Alain Alberto <alainalberto03@gmail.com>
2023-10-01 20:50:57 +0000

    Modificacion partner

--- models/partner.py
+++ models/partner.py
@@ -2,6 +2,6 @@ from odoo import models, fields
 
 
 class ResPartner(models.Model):
-     _inhert = 'res.partner'
+     _inherit = 'res.partner'
 
      x_cliente_ssn = fields.Char(string="Social Security Number")
