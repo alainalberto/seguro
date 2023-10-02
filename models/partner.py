8d87482566cee2e21c8a318faab4b43bd8a7c69d
Alain Alberto <alainalberto03@gmail.com>
2023-10-02 00:47:15 +0000

    Importaciones

--- models/partner.py
+++ models/partner.py
@@ -1,9 +1,17 @@
-from odoo import models, fields
+import time
+import logging
 
+from psycopg2 import sql, DatabaseError
+
+from odoo import api, fields, models, _
+from odoo.osv import expression
+from odoo.tools import DEFAULT_SERVER_DATETIME_FORMAT, mute_logger
+from odoo.exceptions import ValidationError, UserError
+from odoo.addons.base.models.res_partner import WARNING_MESSAGE, WARNING_HELP
 
 class ResPartner(models.Model):
      _name = 'res.partner'
-     _inhert = ['res.partner','partner']
+     _inhert = ['partner']
 
      x_cliente_ssn = fields.Char(string="Social Security Number")
      x_cliente_ein = fields.Char(string="EIN")
