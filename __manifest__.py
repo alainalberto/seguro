cc19b7039b7c0701d9fe97a70cd55953984a81b1
Alain Alberto <alainalberto03@gmail.com>
2023-10-03 04:11:53 +0000

    V10

--- __manifest__.py
+++ __manifest__.py
@@ -30,8 +30,8 @@
     'summary': 'Short summary.',
     'depends': ['account','base_setup', 'base', 'contacts'],
     'data': [
-        'account_customer_ext/views/webclient_templates.xml',
-        'account_customer_ext/views/res_partner.xml',
+        '/views/webclient_templates.xml',
+        '/views/res_partner.xml',
 
      ],
     'images': [],
